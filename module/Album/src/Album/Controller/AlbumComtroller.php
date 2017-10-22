c50ff080087324a26be6065ea3a0943f2ef52bf9
phongvt3110 <vtphong651043@gmail.com>
2017-10-22 12:19:40 +0700

    update code

---
+++ module/Album/src/Album/Controller/AlbumComtroller.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phongvt
+ * Date: 22/10/2017
+ * Time: 09:23
+ */
+
+namespace Album\Controller;
+
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class AlbumComtroller extends AbstractActionController
+{
+
+}
